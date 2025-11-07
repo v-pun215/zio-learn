bc341e544e0c34a6c918fd054b4dc60b827a1fb8
Vihaan Pundir <vihaan.pundir26@gmail.com>
2025-11-07 20:11:16 +0000

    dynamic programming fibonnaci

--- fib.py
+++ fib.py
@@ -1,3 +1,7 @@
+import sys
+sys.set_int_max_str_digits(10000000)
+
+cache=[]
 def fibonnaci(num):
     if num == 0 or num == 1:
         return num
@@ -8,5 +12,21 @@ def fibonnaci(num):
     value = fibonnaci(num-2) + fibonnaci(num-1)
     cache.append([num,value])
     return value
+def test_normal_fibonnaci():
+
+    print(fibonnaci(40000))
+
+def dp_fib(num):
+    if len(cache)<1:
+        cache.append(0)
+    if len(cache)<2:
+        cache.append(1)
+    if num<=0:
+        return 0
+    for i in range(len(cache), num+1):
+        cache.append(cache[i-1] + cache[i-2])
+
+    return cache[num]
+print(dp_fib(40000))
 cache=[]
-print(fibonnaci(400))
+test_normal_fibonnaci()
